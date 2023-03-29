4d3721f590171f1d4b24814c41f8f8496e4adf65
fmitei <faith.mitei@trustgro.com>
2023-03-29 10:43:08 +0300

    completed assignment

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-/^\+?\d{1,3}[-\s]?\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}$/.match?(phone)
+# /^\+?\d{1,3}[-\s]?\(?\d{3}\)?[-\s]?\d{3}[-\s]?\d{4}$/.match?(phone)
+/\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/.match?(phone)
 end
